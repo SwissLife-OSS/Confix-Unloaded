108acce88e01a2d3e58b31e92538e6580f95aa86
Philippe <tree@gmx.ch>
2021-01-20 13:36:51 +0100

    Create base editor

--- src/UI/confix-ui/src/components/Editors/VariableEditor.vue
+++ src/UI/confix-ui/src/components/Editors/VariableEditor.vue
@@ -1,66 +1,60 @@
 <template>
-  <v-card class="mt-2" elevation="1" rounded="0">
-    <v-toolbar height="36" color="amber lighten-4" light>
-      <v-toolbar-title
-        ><v-icon color="black" class="mr-2">mdi-variable</v-icon
-        >{{ variable.name }}
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-icon class="mx-2" @click="setView('FORM')" disabled
-        >mdi-form-textbox</v-icon
-      >
-      <v-icon class="mx-2" @click="setView('LIST')"
-        >mdi-format-list-bulleted-square</v-icon
-      >
-      <v-icon class="mr-0" @click="setView('EDIT')">mdi-pencil</v-icon>
-    </v-toolbar>
-    <v-card-text>
-      <v-row>
-        <v-col md="6"
-          ><v-select
-            label="Application"
-            :items="applications"
-            prepend-icon="mdi-package-variant"
-            v-model="application"
-            item-text="name"
-            item-value="id"
-            return-object
-          ></v-select
-        ></v-col>
-        <v-col md="6"
-          ><v-select
-            label="Parts"
-            prepend-icon="mdi-toy-brick-outline"
-            :items="parts"
-            v-model="part"
-            item-text="name"
-            item-value="id"
-            return-object
-          ></v-select
-        ></v-col>
-      </v-row>
+  <editor-base
+    :title="variable.name"
+    color="amber lighten-4"
+    icon="mdi-variable"
+  >
+    <slot>
+      <template>
+        <v-row>
+          <v-col md="6"
+            ><v-select
+              label="Application"
+              :items="applications"
+              prepend-icon="mdi-package-variant"
+              v-model="application"
+              item-text="name"
+              item-value="id"
+              return-object
+            ></v-select
+          ></v-col>
+          <v-col md="6"
+            ><v-select
+              label="Parts"
+              prepend-icon="mdi-toy-brick-outline"
+              :items="parts"
+              v-model="part"
+              item-text="name"
+              item-value="id"
+              return-object
+            ></v-select
+          ></v-col>
+        </v-row>
 
-      <v-row v-for="vv in variableValues" :key="vv.environmentId">
-        <v-col md="1">
-          <h4 class="mt-6">{{ vv.environment }}</h4>
-        </v-col>
-        <v-col md="11">
-          <v-text-field
-            v-model="vv.value"
-            clearable
-            clear-icon="mdi-delete-variant"
-            append-icon="mdi-check"
-            @click:append="onSave(vv)"
-          ></v-text-field>
-        </v-col>
-      </v-row>
-    </v-card-text>
-  </v-card>
+        <v-row v-for="vv in variableValues" :key="getKey(vv)">
+          <v-col md="1">
+            <h4 class="mt-6">{{ vv.environment }}</h4>
+          </v-col>
+          <v-col md="11">
+            <v-text-field
+              v-model="vv.value"
+              clearable
+              clear-icon="mdi-delete-variant"
+              append-icon="mdi-check"
+              @click:append="onSave(vv)"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </template>
+    </slot>
+  </editor-base>
 </template>
 
 <script>
 import { mapActions } from "vuex";
+import EditorBase from "../Shell/EditorBase.vue";
 export default {
+  components: { EditorBase },
   props: ["variable"],
   data() {
     return {
@@ -91,22 +85,21 @@ export default {
 
       for (let i = 0; i < values.length; i++) {
         const env = values[i];
+        env.value = "";
+        env.id = null;
         const vv = this.variable.values.find(
           (x) =>
             x.environmentId === env.environmentId &&
             x.applicationId === this.selectedApplicationId &&
             x.partId === this.selectedPartId
         );
-        console.log(env.environmentId, this.selectedPartId, vv);
         if (vv != null) {
           env.value = vv.value;
           env.id = vv.id;
-        } else {
-          env.value = null;
-          env.id = null;
         }
       }
 
+      console.log(values);
       return values;
     },
     applications: function () {
@@ -135,6 +128,11 @@ export default {
   },
   methods: {
     ...mapActions("vars", ["saveValue"]),
+    getKey: function (value) {
+      return `${this.variableId}_${value.environmentId}_${
+        this.selectedApplicationId ?? "G"
+      }_${this.selectedPartId ?? "G"}`;
+    },
     onSave: function (value) {
       this.saveValue({
         variableId: this.variable.id,
