1bdd9d08d162bbbe285dcc67c13d7e3dbebdd61d
Michael Staib <michael@chillicream.com>
2021-07-27 08:22:59 +0200

    Renamed ContextData

--- src/Authoring/src/Authoring.GraphQL/Errors/ErrorContextData.cs
+++ src/Authoring/src/Authoring.GraphQL/Errors/ErrorContextData.cs
@@ -2,8 +2,8 @@ namespace Confix.Authoring.GraphQL
 {
     internal static class ErrorContextData
     {
-        public const string ErrorDefinitions = "SwissLife.Confix.ErrorDefinitions";
+        public const string ErrorDefinitions = "Confix.ErrorDefinitions";
 
-        public const string Errors = "SwissLife.Confix.Errors";
+        public const string Errors = "Confix.Errors";
     }
 }
