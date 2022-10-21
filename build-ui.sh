1caa1f660b51866b6471e08a3699c2e6ea80115c
PascalSenn <senn.pasc@gmail.com>
2022-10-21 12:53:21 +0200

    Fixed release (#39)

--- build-ui.sh
+++ build-ui.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 rm -rf ./src/Backend/src/Authoring.UI/UI/
+yarn --cwd ./src/Frontend
 yarn --cwd ./src/Frontend build
 cp -R ./src/Frontend/build ./src/Backend/src/Authoring.UI/UI
 touch ./src/Backend/src/Authoring.UI/UI/.keep
