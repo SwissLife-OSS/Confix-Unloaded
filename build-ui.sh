d2b01d8c39f45e181f1c1be0594ed4a617046e03
PascalSenn <senn.pasc@gmail.com>
2022-10-24 14:38:10 +0200

    Fixed embedded resources (#44)

--- build-ui.sh
+++ build-ui.sh
@@ -2,5 +2,5 @@
 rm -rf ./src/Backend/src/Authoring.UI/UI/
 yarn --cwd ./src/Frontend
 yarn --cwd ./src/Frontend build
-cp -R ./src/Frontend/build ./src/Backend/src/Authoring.UI/UI
+cp -R ./src/Frontend/build/* ./src/Backend/src/Authoring.UI/UI
 touch ./src/Backend/src/Authoring.UI/UI/.keep
