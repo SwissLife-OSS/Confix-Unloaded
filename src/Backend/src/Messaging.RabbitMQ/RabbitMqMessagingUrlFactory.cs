45462b4c247e8569d67df68db9880c6b6a5dd028
Pascal Senn <senn.pasc@gmail.com>
2023-02-28 15:40:44 +0100

    Fixed format

--- src/Backend/src/Messaging.RabbitMQ/RabbitMqMessagingUrlFactory.cs
+++ src/Backend/src/Messaging.RabbitMQ/RabbitMqMessagingUrlFactory.cs
@@ -5,6 +5,5 @@ internal class RabbitMqMessagingUrlFactory : IMessagingUrlFactory
     public Uri CreateRequestClientUrl(string name, string environment)
     {
         return new Uri($"exchange:{name}-{environment}");
-        //return new UriBuilder(_options.HostUrl) { Path = $"/{topic}-{environment}" }.Uri;
     }
 }
