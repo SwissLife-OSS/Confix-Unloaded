47971a1c2d5437167a13ed1cff3ddc2f7843f1ad
Florian Rohrer <florian.rohrer@swisslife.ch>
2023-04-03 13:57:17 +0200

    fix ui build

--- src/Frontend/src/settings/groups/EditGroup.tsx
+++ src/Frontend/src/settings/groups/EditGroup.tsx
@@ -1,31 +1,33 @@
-import { useFragment, useLazyLoadQuery, useMutation } from "react-relay";
 import { Button, Card, Col, List, Modal, Row, Select } from "antd";
-import { DetailView } from "../../shared/DetailView";
-import { graphql } from "babel-plugin-relay/macro";
-import { EditGroupQuery } from "./__generated__/EditGroupQuery.graphql";
-import { EditableBreadcrumbHeader } from "../../shared/EditablePageHeader";
-import { useToggle } from "../../shared/useToggle";
-import { RenameGroupDialog } from "./controls/dialogs/RenameGroupDialog";
-import React, { useState } from "react";
-import { EditGroup_Group$key } from "./__generated__/EditGroup_Group.graphql";
-import { css } from "@emotion/react";
-import { useParams } from "react-router";
-import { useHandler } from "../../shared/useHandler";
 import { FieldInput, FormActions } from "../../shared/FormField";
-import { EditGroup_UpdateGroupRoles_Mutation } from "./__generated__/EditGroup_UpdateGroupRoles_Mutation.graphql";
-import { EditGroup_UpdateGroupRequirements_Mutation } from "./__generated__/EditGroup_UpdateGroupRequirements_Mutation.graphql";
-import { id } from "../../shared/id";
-import { DeleteIcon } from "../../icons/icons";
-import { SectionHeader } from "../../shared/SectionHeader";
+import React, { useState } from "react";
+import { RoleScopeData, RoleScopeEditor } from "../shared/RoleScopeEdit";
 import {
   pipeCommitFn,
   withErrorNotifications,
   withSuccessMessage,
 } from "../../shared/pipeCommitFn";
-import { RoleScopeData, RoleScopeEditor } from "../shared/RoleScopeEdit";
+import { useFragment, useLazyLoadQuery, useMutation } from "react-relay";
+
+import { DeleteIcon } from "../../icons/icons";
+import { DetailView } from "../../shared/DetailView";
+import { EditGroupQuery } from "./__generated__/EditGroupQuery.graphql";
 import { EditGroup_Form$key } from "./__generated__/EditGroup_Form.graphql";
-import { EditGroup_RoleScopeSection$key } from "./__generated__/EditGroup_RoleScopeSection.graphql";
+import { EditGroup_Group$key } from "./__generated__/EditGroup_Group.graphql";
+import { EditGroup_Header$key } from "./__generated__/EditGroup_Header.graphql";
 import { EditGroup_RequirementsSection$key } from "./__generated__/EditGroup_RequirementsSection.graphql";
+import { EditGroup_RoleScopeSection$key } from "./__generated__/EditGroup_RoleScopeSection.graphql";
+import { EditGroup_UpdateGroupRequirements_Mutation } from "./__generated__/EditGroup_UpdateGroupRequirements_Mutation.graphql";
+import { EditGroup_UpdateGroupRoles_Mutation } from "./__generated__/EditGroup_UpdateGroupRoles_Mutation.graphql";
+import { EditableBreadcrumbHeader } from "../../shared/EditablePageHeader";
+import { RenameGroupDialog } from "./controls/dialogs/RenameGroupDialog";
+import { SectionHeader } from "../../shared/SectionHeader";
+import { css } from "@emotion/react";
+import { graphql } from "babel-plugin-relay/macro";
+import { id } from "../../shared/id";
+import { useHandler } from "../../shared/useHandler";
+import { useParams } from "react-router";
+import { useToggle } from "../../shared/useToggle";
 
 export const EditGroup = () => {
   const { groupId = "" } = useParams();
