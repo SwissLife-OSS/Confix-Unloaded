1b311cd4a1279cea22a9f60703398a01644de9ed
Florian Rohrer <florian.rohrer@swisslife.ch>
2023-05-08 09:09:51 +0200

    ComponentById_Should_OnlyReturnComponentsWithReadPermission

--- src/Backend/test/Authoring.Integration.Tests/ComponentTests.cs
+++ src/Backend/test/Authoring.Integration.Tests/ComponentTests.cs
@@ -36,6 +36,35 @@ public class ComponentTests : IClassFixture<MongoResource>
 
         // assert
         result.AssertNoErrors();
-        result.Data.MatchSnapshot();
+        result.Data!.Components!.Nodes.MatchSnapshot();
+    }
+
+    [Fact]
+    public async Task ComponentById_Should_OnlyReturnComponentsWithReadPermission()
+    {
+        // arrange
+        await TestExecutorBuilder
+            .New()
+            .AddDatabase(_mongoResource.ConnectionString)
+            .AddDefaultUser()
+            .AddPermission(Namespaces.Default, Scope.Component, Permissions.Read)
+            .AddPermission(Namespaces.Other, Scope.Application, Permissions.Read)
+            .AddClient(out var client)
+            .SetupDb(db => db
+                .AddComponent(x => x with { Id = Guid.Parse("10000000-0000-0000-0000-000000000000"), Name = "Component 1" })
+                .AddComponent(x => x with
+                {
+                    Id = Guid.Parse("20000000-0000-0000-0000-000000000000"),
+                    Name = "Component 2",
+                    Scopes = new[] { new ComponentScope(Namespaces.Other, null, null) }
+                }))
+            .Build();
+
+        // act
+        var result = await client.Value.GetComponents.ExecuteAsync();
+
+        // assert
+        result.AssertNoErrors();
+        Assert.Equal(1, result.Data!.Components!.Nodes!.Count);
     }
 }
