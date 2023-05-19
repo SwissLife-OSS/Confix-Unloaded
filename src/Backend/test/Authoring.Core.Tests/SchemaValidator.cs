23434b609e609b257c4d39fb05677e8204c438d9
Florian Rohrer <florian.rohrer@swisslife.ch>
2023-05-19 09:04:25 +0200

    Validate Schema Tests

--- src/Backend/test/Authoring.Core.Tests/SchemaValidator.cs
+++ src/Backend/test/Authoring.Core.Tests/SchemaValidator.cs
@@ -1,30 +1,107 @@
-using Squadron;
-using Xunit;
 using Microsoft.Extensions.Caching.Memory;
-using  Confix.Authoring.Internal;
+using Confix.Authoring.Internal;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Confix.Authoring.Core.Tests;
 
 public class SchemaValidatorTests
 {
-    [Theory]
-    [InlineData(
-        """
-        type Component {
+    [Fact]
+    public void ValidateSchema_Primitives_Valid()
+        => AssertValidSchema("""
+            type Configuration {
+                someText: String!
+                someNumber: Int!
+                someFloat: Float!
+                someLong: Long!
+                someBoolean: Boolean!
+            }
+        """);
+
+    [Fact]
+    public void ValidateSchema_Enums_Valid()
+        => AssertValidSchema("""
+            type Configuration {
+                someText: String!
+                someEnum: ConfigurationEnum!
+            }
+
+            enum ConfigurationEnum {
+                OPTION_1
+                OPTION_2
+                OPTION_3
+            }
+            """);
+
+    [Fact]
+    public void ValidateSchema_Arrays_Valid()
+    => AssertValidSchema("""
+        type Configuration {
             someText: String!
-            someNumber: Int!
-            someDouble: Double!
-            someBoolean: Boolean!
+            someNumbers: [Int!]!
+            someStrings: [String]!
+            someCustom: [Custom!]
+            someCustom2: [Custom]!
+            someCustom3: [Custom!]!
         }
-        """)]
-    public void ValidateSchema_ValidSchemas_NoException(string schema)
+
+        type Custom {
+            unicornCount: Int!
+        }
+        """);
+
+    [Fact]
+    public void ValidateSchema_Unions_Valid()
+        => AssertValidSchema("""
+            type Configuration {
+                someField: UnionOfPrimitivesOrCustomTypes!
+            }
+
+            union UnionOfPrimitivesOrCustomTypes = PrimitiveUnion | CustomTypeUnion
+            union PrimitiveUnion = String | Int | Boolean
+            union CustomTypeUnion = CustomType1 | CustomType2
+
+            type CustomType1 {
+                field1: String!
+            }
+
+            type CustomType2 {
+                field2: Int!
+            }
+            """);
+
+    [Fact]
+    public void ValidateSchema_Nested_Valid()
+        => AssertValidSchema("""
+            type Configuration {
+                someText: String!
+                level1: Level1!
+            }
+
+            type Level1 {
+                field1: String!
+                level2: Level2!
+            }
+
+            type Level2 {
+                field2: String!
+                level3: Level3!
+            }
+
+            type Level3 {
+                field3: String!
+            }
+        """);
+
+    private void AssertValidSchema(string schema)
+        => new SchemaValidator(GetMemoryCache()).ValidateSchema(schema);
+
+    private IMemoryCache GetMemoryCache()
     {
-        // Arrange
-        SchemaValidator validator = new(Mock.Of<IMemoryCache>());
+        var services = new ServiceCollection();
+        services.AddMemoryCache();
+        var serviceProvider = services.BuildServiceProvider();
 
-        // Act && Assert
-        validator.ValidateSchema(schema);
+        return serviceProvider.GetRequiredService<IMemoryCache>();
     }
-
-
 }
